fe9a1e1f1fbaea023a50187702b4e91626bea21e
Glitch (hello-express) <none>
2020-05-01 23:32:28 +0000

    ⛰🏋 Checkpoint ./server.js:4090153/1169 ./package.json:4090153/100 ./views/index.html:4090153/2612 ./wasm.js:4090153/117740 ./.glitch-assets:4090153/1422 ./index.html:4090153/5341 ./public/client.js:4090153/3

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-console.log("hello world");
+console.log("hello world :0");
 
 // our default array of dreams
 const dreams = [
--- server.js
+++ server.js
@@ -1,61 +1,11 @@
 const express = require("express");
-const fs = require("fs");
-const {spawn} = require("child_process");
-const {v4: uuidv4} = require("uuid");
 
 const app = express();
-const tempDir = "/tmp/mips";
-fs.mkdirSync(tempDir, {recursive: true});
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
-});
-
-app.get("/spim", (req, res) => {
-  const id = uuidv4();
-  const file = `${tempDir}/${id}`;
-
-  const hello = `
-        .data
-msg:   .asciiz "Hello World"
-	.extern foobar 4
-
-        .text
-        .globl main
-main:   li $v0, 4       # syscall 4 (print_str)
-        la $a0, msg     # argument: string
-        syscall         # print the string
-        lw $t1, foobar
-        
-        jr $ra          # retrun to caller`
-  
-  fs.writeFile(file, hello, (err) => {
-    if (err) {
-      res.send(err);
-      return console.log(err);
-    }
-  
-    const child = spawn(`./spim -f ${file}`);
-    //const child = spawn(`ls`);
-    //process.stdin.pipe(child.stdin);
-    let stdout = "";
-    
-    child.stdout.on("data", data => {
-      stdout += data;
-    });
-    
-    child.on("exit", (code, signal) => {
-      res.send("done: " + stdout);
-    });
-    
-    child.on("error", (code, signal) => {
-      res.send("fail: " + code + " " +signal);
-    });
-    
-    //fs.unlink temp file
-  }); 
 });
 
 const listener = app.listen(process.env.PORT, () =>
