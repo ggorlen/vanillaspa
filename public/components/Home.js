20d3206a879f11f6b86a52ff13dea9509c6aea4a
Glitch (softer) <none>
2020-10-03 21:22:18 +0000

    🏯💃 Checkpoint ./public/components/Home.js:4090153/968 ./public/style.css:4090153/85 ./public/index.js:4090153/928

--- public/components/Home.js
+++ public/components/Home.js
@@ -1,10 +1,18 @@
 const Home = () => {
+  fetch("https://api.github.com/gists/public")
+    .then(res => res.json())
+    .then(data => {
+      const gistsEl = document.querySelector("#gists");
+      data.forEach(e => gistsEl.e)
+    })
+  ;
   
   return `
     ${Header()}
     <h1>home</h1>
     <main>
-      Hello world...
+      <p>Recent github gists:</p>
+      <div id="gists">loading...</div>
     </main>
   `;
 };
