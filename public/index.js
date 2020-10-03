232e11e194af1018f93d00256345b9de46ff71a8
Glitch (softer) <none>
2020-10-03 20:12:09 +0000

    🍫🎺 Checkpoint ./public/index.js:4090153/2822

--- public/index.js
+++ public/index.js
@@ -1,45 +1,36 @@
 (() => {
-
+  
+  const appEl = document.querySelector("#app");
+  
   const routes = {
     "": Home,
     "about": About,
   };
   
-  const appEl = document.querySelector("#app");
-  const appEl = document.querySelector("#app");
+  const pushHandler = event => {
+    let id = event.target.id;
+    window.history.pushState({id}, `${id}`, `/${id}`);
+  };
   
-  const path = window.location.href.split("/");
-  const page = path[path.length-1];
-  console.log(window.location.href)
-  console.log(path)
-                window["home"].addEventListener("click", event => push(event))
-                window["about"].addEventListener("click", event => push(event))
-                window["gallery"].addEventListener("click", event => push(event))
-                window["contact"].addEventListener("click", event => push(event))
-  
-              function push(event) {
-                // Get id attribute of the box or button or link clicked
-                let id = event.target.id;
-                // Visually select the clicked button/tab/box
-                select_tab(id);
-                // Update Title in Window's Tab
-                document.title = id.charAt(0).toUpperCase() + id.slice(1);
-                // Load content for this tab/page
-                load_content(id);
-                // Finally push state change to the address bar
-                window.history.pushState({id}, `${id}`, `/${id}`);
-            }
-  if (routes[page]) {
-    appEl.innerHTML = routes[page]();
-  }
-  else {
-    appEl.innerHTML = "Page not found";
-  }
+  const navigate = page => {
+    appEl.innerHTML = routes[page] ? routes[page]() : "Page not found";
+    [...document.querySelectorAll(".navigable")].forEach(el => 
+      //el.removEventListener("click", e => {
+      el.addEventListener("click", e => {
+        console.log("navigation")
+      })
+    );
+  };
 
   window.addEventListener("popstate", event => {
     let stateId = event.state.id;
-    select_tab(stateId);
-    // Load content for this tab/page
-    load_content(id);
+   // load_content(id);
   });
+  
+  const path = window.location.href.split("/");
+  const page = path[path.length-1];
+  navigate(page);
+  console.log(window.location.href)
+  console.log(path)
+  // window["contact"].addEventListener("click", event => push(event))
 })();
