eca8c58080916ebe853025f06132ae655b2f868c
Glitch (softer) <none>
2020-10-27 17:23:12 +0000

    💴🐵 Checkpoint ./public/index.js:4090153/558

--- public/index.js
+++ public/index.js
@@ -11,13 +11,9 @@ import Posts from "./components/Posts.js";
   };
   
   const render = (rootEl, path) => {
-    const page = path.match(/^\/[^/]*/g);
-    const pieces = path.match(/[^/]+/g);
-    
-    if (pieces.length > 1) {
-      
-    }
-    console.log(path,page)
+    const chunks = path.split("/");
+    const resource = chunks.length > 2 ? chunks.pop() : null;
+    const page = chunks.join("/");
     const toRender = routes[page] ? routes[page]() : NotFound();
     
     // A component can return either a template string or a DOM element
