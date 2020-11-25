c827b57ad6a9c028b5ad8bf5267ba741dccce8d4
Glitch (softer) <none>
2020-11-25 19:14:09 +0000

    👴🛣 Checkpoint ./public/Router.js:4090153/191

--- public/Router.js
+++ public/Router.js
@@ -13,9 +13,7 @@ const Router = (() => {
     const chunks = path.split("/");
     const resource = chunks.length > 2 ? chunks.pop() : undefined;
     const page = chunks.join("/");
-    const toRender = _routes[page] 
-      ? _routes[page](resource) : _NotFound()
-    ;
+    const toRender = (_routes[page] || _NotFound)(resource);
     
     // A component can return either a template string or a DOM element
     if (typeof toRender === "string") {
