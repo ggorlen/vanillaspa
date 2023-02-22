8003e37080d20734b5d280b2e20bacc880743f90
Glitch (vanillaspa) <none>
2023-02-22 23:05:35 +0000

    💇🌃 Checkpoint ./server.js:4090153/867

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const crypto = require("crypto");
 const fs = require("fs");
 const http = require("http");
 const path = require("path");
 
 const publicDir = path.join(__dirname, "public");
-const staticFiles = new Set([
-  "/", ...fs.readdirSync(publicDir).map(e => `/${e}`)
-]);
 const mimeTypes = {
   ".html": "text/html",
   ".js": "text/javascript",
@@ -20,12 +16,14 @@ const mimeTypes = {
 };
 
 const servePublicFile = (req, res) => {
-  const filePath = publicDir + req.url + 
-                   (req.url.endsWith("/") ? "index.html" : "");
-        console.log(filePath);
+  const filePath = path.join(
+    publicDir,
+    req.url,
+    (req.url.endsWith("/") ? "index.html" : "")
+  );
   const extname = String(path.extname(filePath)).toLowerCase();
   const contentType = mimeTypes[extname] || "application/octet-stream";
-  
+
   fs.readFile(filePath, (error, content) => {
     if (error) {
       if (error.code === "ENOENT") {
@@ -44,9 +42,14 @@ const servePublicFile = (req, res) => {
   });
 };
 const onRequest = (req, res) => {
-  console.log(req.url);
-  if (req.method === "GET" && staticFiles.has(req.url)) {
-    console.log("K")
+  const routes = ["about", "gists"];
+  const resource = req.url.replace(/^\//, "").split("/")[0];
+console.log(resource);
+  if (req.method === "GET") {
+    if (routes.includes(resource)) {
+      req.url = "/";
+      console.log("  K")
+    }
     return servePublicFile(req, res);
   }
 
