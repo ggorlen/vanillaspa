72a6c380dedfbb4f9d04c7ab9d3c1741cc3dc8b0
ggorlen <gsgorlen@gmail.com>
2025-06-07 22:16:29 -0700

    Undo router changes

--- Router.js
+++ Router.js
@@ -28,7 +28,7 @@ const Router = (() => {
     _rootEl.querySelectorAll('[href^="/"]').forEach(el => 
       el.addEventListener("click", evt => {
         evt.preventDefault();
-        redirect("/vanillaspa" + new URL(evt.target.href).pathname);
+        redirect(new URL(evt.target.href).pathname);
       })
     );
   };
