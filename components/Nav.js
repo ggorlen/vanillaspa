77dc421575b8a7bd6edae366074e02695c2a98c9
ggorlen <gsgorlen@gmail.com>
2025-06-07 22:24:28 -0700

    Update URLs

--- components/Nav.js
+++ components/Nav.js
@@ -2,9 +2,9 @@ export default () => `
   <nav>
     <ul>
       <li>Vanilla SPA experiment</li>
-      <li><a href="/">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/gists">Gists</a></li>
+      <li><a href="/vanillaspa">Home</a></li>
+      <li><a href="/vanillaspa/about">About</a></li>
+      <li><a href="/vanillaspa/gists">Gists</a></li>
     </ul>
   </nav>
 `;
