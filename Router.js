c2d4d3c9895c9480327ddb5af5e56289b798ad83
ggorlen <gsgorlen@gmail.com>
2025-06-07 22:33:27 -0700

    Remove debug

--- Router.js
+++ Router.js
@@ -12,8 +12,6 @@ const Router = (() => {
   const render = path => {
     const chunks = path.split("/");
     const resource = chunks.length > 3 ? chunks.pop() : undefined;
-    const page = chunks.join("/");
-    console.log(page, _routes, chunks, resource);
     const toRender = (_routes[page] || _NotFound)(resource);
     
     // A component can return either a template string or a DOM element
